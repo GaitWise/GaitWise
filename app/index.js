9acb0da3c617c5c1b520b0fb4290b33cf9def0ec
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-09-22 04:22:06 +0900

    Navigation

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { Text, View } from "react-native-web";
+import { useRouter } from "expo-router";
+
+const Splash = () => {
+  const router = useRouter(); 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/main"); 
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Splash</Text>
+    </View>
+  );
+}
+
+export default Splash;
---
+++ app/main/board.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native-web";
+
+const Board = () => {
+  return (
+    <View>
+      <Text>Board</Text>
+    </View>
+  )
+}
+
+export default Board
