e39ab0eda0589d85e01bb4a974567b6c91e58b2a
Hisoo038 <gkswltn38@naver.com>
2024-09-26 20:03:19 +0900

    feat : COLORS name 수정 - 코드 색 이름 수정함

--- constants/theme.js
+++ constants/theme.js
@@ -1,10 +1,12 @@
 const COLORS ={
-    color1 :  "#27187E",
-    color2 : "#758BFD",
-    color3 : "#AEB8FE",
-    color4 : "#F1F2F6",
-    color5 : "#1C2A3A",
-    color6 : "#363B59",
-    black: "#0000",
-
-}
+    dark_indigo :  "#27187E",
+    soft_blue : "#758BFD",
+    pastel_lavender : "#AEB8FE",
+    light_mist_grey : "#F1F2F6",
+    deep_slate_blue : "#1C2A3A",
+    charcoal_blue : "#363B59",
+    bright_pink : "#F163AC",
+    light_gray : "#D9D9D9",
+    pale_gray: "#E8E8E8",
+    black : "#0000",
+};
