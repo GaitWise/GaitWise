a64c894a7dd229f371fb344e80cd7c83b9b115db
Hisoo038 <gkswltn38@naver.com>
2024-10-03 00:49:56 +0900

    style🎨: _layout.js 코드 수정

--- app/survey/_layout.js
+++ app/survey/_layout.js
@@ -1,54 +1,38 @@
+import { icons } from "../../constants"; 
 import { Stack, useRouter } from "expo-router";
-import * as React from "react";
-import { TouchableOpacity } from "react-native";
-import { Image, StyleSheet, Text, View, Pressable } from "react-native";
-import { COLORS, icons, images, SIZES } from "../../constants";
+import { TouchableOpacity, Image, StyleSheet } from "react-native";
 
-const SurveyLayout = () => {
+export function SurveyLayout() {
   const router = useRouter();
 
-  // 뒤로 가기 버튼 UI
+  // 뒤로 가기 버튼 UI 
   const renderBackButton = () => (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => router.back()}
-    ></TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={() => router.back()}>
+      <Image source={icons.arrow_back}/>
+    </TouchableOpacity>
   );
 
-  // // 분석가와 모니터링 연결 버튼 UI
-  // const renderConnectButton = () => (
-  //   <TouchableOpacity
-  //     style={styles.button}
-  //     onPress={() => console.log("Connect Button")}
-  //   ></TouchableOpacity>
-  // );
   return (
     <Stack
       screenOptions={{
-        title: "Walking",
-        headerTitleAlign: "center",
-        headerTitleStyle: { fontSize: 16 },
+        title: "Back",
+        headerTitleStyle: { fontSize: 20},
         headerLeft: renderBackButton,
-        // headerRight: renderConnectButton,
       }}
     />
   );
-};
-
-export default SurveyLayout;
+}
 
 const styles = StyleSheet.create({
   button: {
     flexDirection: "row",
     alignItems: "center",
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   backicon: {
     width: 24,
     height: 16,
-  },
-  connecticon: {
-    width: 24,
-    height: 24,
-  },
+  }
 });
+
+export default SurveyLayout
