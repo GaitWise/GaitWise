6bd1b96e1cbb397401e4180e5778013fe8272bfb
Hisoo038 <gkswltn38@naver.com>
2024-10-02 22:57:23 +0900

    style🎨: gender 수정 - styled components 사용 - svg 파일 수정

--- app/survey/gender.js
+++ app/survey/gender.js
@@ -1,298 +1,161 @@
 import * as React from "react";
+import styled from "styled-components/native";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Image, StyleSheet, Text, View, Pressable } from "react-native";
-import { COLORS, icons, images, SIZES } from "../../constants";
+import { Image, Pressable, Text } from "react-native";
+import { COLORS, icons } from "../../constants";
 import { useNavigation } from "expo-router";
 
 const AGender = () => {
-  const navigation = useNavigation()
-  const [selectedGender, setSelectedGender] = useState(null)
-  const handleGenderSelect = (gender) => {
-    setSelectedGender(gender)
-  }
+  const navigation = useNavigation();
+  const [selectedMale, setSelectedMale] = useState(true);
+  const [selectedFemale, setSelectedFemale] = useState(true);
+
+  const handleMaleSelect = () => {
+    setSelectedMale(true);
+    setSelectedFemale(false);
+  };
+
+  const handleFemaleSelect = () => {
+    setSelectedFemale(true);
+    setSelectedMale(false);
+  };
 
   return (
-    <Pressable style={styles.aGender} onPress={() => {}}>
-      <View style={styles.statusbarParent}>
-        <Pressable style={styles.titlebackIcon} onPress={() => {}}>
-          <Pressable style={styles.arrowLeftbackIcon}>
-            <Image
-              style={styles.icon}
-              resizeMode="center"
-              source={icons.arrow_back}
-            />
-          </Pressable>
-          <Text style={styles.fillYourProfile}>back</Text>
-        </Pressable>
-      </View>
-      <View style={styles.whatsYourGenderParent}>
-        <Text style={[styles.whatsYourGender, styles.maleTypo]}>
-          What’s Your Gender
-        </Text>
-        <View style={styles.view}>
-          <TouchableOpacity style={styles.botGenderMaleParent} onPress={()=>handleGenderSelect("female")}>   
-            <View style={styles.botGenderMale}>
-              <View style={[styles.gradientButton, styles.gradientPosition]}>
-                <View
-                  style={[styles.gradientButtonChild, styles.gradientPosition, selectedGender ==="male" && styles.selectedIcon]}
-                />
-                <View
-                  style={[styles.gradientButtonItem, styles.gradientPosition]}
-                />
-              </View>
-              <Image
-                style={[styles.vectorIcon, selectedGender === "male" && styles.selectedIcon]}
-                resizeMode="center"
-                source={icons.male}
-              />
-            </View>
-            <Text style={[styles.male, styles.maleFlexBox]}>Male</Text>
-          </TouchableOpacity>   
-          <TouchableOpacity style={styles.botGenderFemaleParent} onPress={()=>handleGenderSelect("male")}>  
-            <View style={styles.botGenderFemale}>
-              <View style={[styles.gradientButton, styles.gradientPosition]}>
-                <View
-                  style={[styles.gradientButtonChild2, styles.gradientPosition, selectedGender ==="female" && styles.selectedIcon]}
-                />
-                <View
-                  style={[styles.gradientButtonItem, styles.gradientPosition]}
-                />
-              </View>
-              <Image
-                style={[styles.vectorIcon2, selectedGender === "female" && styles.selectedIcon]}
-                resizeMode="center"
-                source={icons.female}
-              />
-            </View>
-            <Text style={[styles.female, styles.femaleFlexBox]}>Female</Text>
-          </TouchableOpacity>   
-        </View>
-        <TouchableOpacity style={[styles.continue, styles.femaleFlexBox, selectedGender&&styles.seledted1]} disabled={!selectedGender} onPress={()=>navigation.navigate('age')}>
-            <Text style={[styles.continue1, styles.maleTypo]}>Continue</Text>
-        </TouchableOpacity>
-      </View>
-    </Pressable>
+    <GenderContainer onPress={() => {}}>
+      <StatusBarContainer>
+        <BackIcon onPress={() => {}} />
+      </StatusBarContainer>
+      <GenderContent>
+        <TitleText>What’s Your Gender</TitleText>
+        <GenderSelection>
+          {/* 남 버튼 */}
+          <GenderButton onPress={handleMaleSelect}>
+            <MaleIconContainer selected={selectedMale}>
+              <GenderIcon source={icons.male} />
+            </MaleIconContainer>
+            <GenderLabel>Male</GenderLabel>
+          </GenderButton>
+          {/* 여 버튼 */}
+          <GenderButton onPress={handleFemaleSelect}>
+            <FemaleIconContainer selected={selectedFemale}>
+              <GenderIcon source={icons.female} />
+            </FemaleIconContainer>
+            <GenderLabel>Female</GenderLabel>
+          </GenderButton>
+        </GenderSelection>
+        <ContinueButton
+          disabled={selectedMale && selectedFemale}
+          selected={selectedMale || selectedFemale}
+          bothSelected={selectedMale && selectedFemale}
+          onPress={() => navigation.navigate("age")}
+        >
+          <ContinueText>Continue</ContinueText>
+        </ContinueButton>
+      </GenderContent>
+    </GenderContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  timePosition: {
-    top: "50%",
-    position: "absolute",
-  },
-  timeClr: {
-    color: "#000",
-    textAlign: "center",
-  },
-  maleTypo: {
-    fontFamily: "Poppins-Bold",
-    fontWeight: "700",
-    textTransform: "capitalize",
-  },
-  gradientPosition: {
-    bottom: "0%",
-    left: "0%",
-    right: "0%",
-    top: "0%",
-    height: "100%",
-    position: "absolute",
-    width: "100%",
-  },
-  maleFlexBox: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  femaleFlexBox: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  icon: {
-    height: "100%",
-    width: "100%",
-  },
-  arrowLeftbackIcon: {
-    height: 24,
-    width: 24,
-  },
-  fillYourProfile: {
-    lineHeight: 30,
-    fontWeight: "600",
-    fontFamily: "Inter-SemiBold",
-    color: "#374151",
-    textAlign: "left",
-    fontSize: 20,
-  },
-  titlebackIcon: {
-    paddingLeft: 24,
-    gap: 14,
-    flexDirection: "row",
-    alignSelf: "stretch",
-    alignItems: "center",
-  },
-  statusbarParent: {
-    marginTop: 51,
-    alignSelf: "stretch",
-    alignItems: "center",
-  },
-  whatsYourGender: {
-    fontSize: 25,
-    color: "#27187e",
-    textAlign: "left",
-    fontWeight: "bold",
-  },
-  gradientButtonChild: {
-    backgroundColor: COLORS.soft_blue,
-    borderRadius: 100,
-  },
-  gradientButtonChild2: {
-    backgroundColor: COLORS.bright_pink,
-    borderRadius: 100,
-  },
-  seledted1:{
-    backgroundColor: COLORS.dark_indigo,
-  },
-  selectedIcon:{
-    backgroundColor: COLORS.continue_gray,
-  },
-  gradientButtonItem: {
-    borderColor: COLORS.light_mist_grey,
-    borderRadius: 100,
-    borderWidth: 0,
-    borderStyle: "solid",
-    bottom: "0%",
-    shadowRadius: 0,
-  },
-  gradientButton: {
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowRadius: 4,
-    elevation: 4,
-    shadowOpacity: 0,
-  },
-  vectorIcon: {
-    height: 100,
-    width: 100,
-    top: "30%",
-    right: "30%",
-    bottom: "30%",
-    left: "30%",
-    maxWidth: "100%",
-    maxHeight: "100%",
-    position: "static",
-  },
-  vectorIcon2: {
-    height: 100,
-    width: 100,
-    top: "30%",
-    right: "30%",
-    bottom: "30%",
-    left: "30%",
-    maxWidth: "100%",
-    maxHeight: "100%",
-    position: "static",
-  },
-  botGenderMale: {
-    height: 163,
-    width: 163,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  botGenderFemale:{
-    height: 163,
-    width: 163,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  male: {
-    display: "flex",
-    height: 26,
-    fontFamily: "Poppins-Bold",
-    fontWeight: "700",
-    textTransform: "capitalize",
-    fontSize: 20,
-    textAlign: "center",
-    color: "#000",
-    alignSelf: "stretch",
-  },
-  female: {
-    display: "flex",
-    height: 26,
-    fontFamily: "Poppins-Bold",
-    fontWeight: "700",
-    textTransform: "capitalize",
-    fontSize: 20,
-    textAlign: "center",
-    color: "#000",
-    alignSelf: "stretch",
-  },
-  botGenderMaleParent: {
-    height: "44.21%",
-    bottom: "55.79%",
-    left: "0%",
-    right: "0%",
-    top: "0%",
-    position: "absolute",
-    gap: 7,
-    alignItems: "center",
-    width: "100%",
-    alignItems: "center",
-  },
-  botGenderFemaleParent: {
-    height: "44.21%",
-    width: "99.45%",
-    top: "56.67%",
-    right: "0.26%",
-    bottom: "55.79%",
-    left: "0.29%",
-    gap: 7,
-    position: "absolute",
-    alignItems: "center",
-  },
-  view: {
-    height: 450,
-    width: 163,
-  },
-  continue1: {
-    fontSize: 18,
-    color: "#fff",
-    textAlign: "center",
-  },
-  continue: {
-    backgroundColor: COLORS.continue_gray,
-    width: 179,
-    paddingHorizontal: 36,
-    paddingVertical: 10,
-    borderRadius: 100,
-    flexDirection: "row",
-    overflow: "hidden",
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#fff",
-    justifyContent: "center",
-  },
-  whatsYourGenderParent: {
-    height: 739,
-    gap: 56,
-    alignSelf: "stretch",
-    alignItems: "center",
-  },
-  aGender: {
-    borderRadius: 54,
-    backgroundColor: "#fff",
-    flex: 1,
-    height: 852,
-    gap: 10,
-    width: "100%",
-    alignItems: "center",
-    overflow: "hidden",
-    borderWidth: 1,
-    borderColor: "#fff",
-    borderStyle: "solid",
-  },
-});
+const GenderContainer = styled.Pressable`
+  flex: 1;
+  align-items: center;
+  background-color: #fff;
+  height: 852px;
+  border-width: 1px;
+  border-color: #fff;
+  overflow: hidden;
+`;
+
+const StatusBarContainer = styled.View`
+  margin-top: 51px;
+  align-items: center;
+  align-self: stretch;
+`;
+
+const BackIcon = styled.Pressable`
+  padding-left: 24px;
+  gap: 14px;
+  flex-direction: row;
+  align-items: center;
+  align-self: stretch;
+`;
+
+const GenderContent = styled.View`
+  height: 739px;
+  gap: 56px;
+  align-items: center;
+`;
+
+const TitleText = styled.Text`
+  font-size: 25px;
+  color: #27187e;
+  font-family: "Poppins-Bold";
+  font-weight: bold;
+  text-align: left;
+`;
+
+const GenderSelection = styled.View`
+  height: 450px;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+const GenderButton = styled(TouchableOpacity)`
+  align-items: center;
+`;
+
+const MaleIconContainer = styled.View`
+  width: 163px;
+  height: 163px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) =>
+    props.selected ? COLORS.soft_blue : COLORS.continue_gray};
+  border-radius: 100px;
+`;
+
+const FemaleIconContainer = styled.View`
+  width: 163px;
+  height: 163px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) =>
+    props.selected ? COLORS.bright_pink : COLORS.continue_gray};
+  border-radius: 100px;
+`;
+
+const GenderIcon = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
+
+const GenderLabel = styled.Text`
+  font-size: 20px;
+  font-family: "Poppins-Bold";
+  font-weight: 700;
+  color: #000;
+  margin-top: 10px;
+`;
+
+const ContinueButton = styled(TouchableOpacity)`
+  width: 179px;
+  padding: 10px 36px;
+  border-radius: 100px;
+  background-color: ${(props) =>
+    props.selected && !props.bothSelected
+      ? COLORS.dark_indigo
+      : COLORS.continue_gray};
+  border-width: 1px;
+  border-color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ContinueText = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-family: "Poppins-Bold";
+`;
 
 export default AGender;
