b9e55a6045f005216d1cf6f2f51e81a7204442e4
Hisoo038 <gkswltn38@naver.com>
2024-11-22 17:42:39 +0900

    refactor♻️: styled-component 수정
    - 버튼 style 수정

--- app/(tabs)/walking/index.js
+++ app/(tabs)/walking/index.js
@@ -1,272 +1,315 @@
 import 'react-native-get-random-values';
-import { icons } from "../../../constants";
-import React, {useCallback, useState} from 'react';
-import { Sensors } from "../../../components/walking/Sensor";
-import { insertSensorData} from "../../../components/walking/SQLite";
-import { sendDatabaseFile } from "../../../components/walking/api/Senddata";
-import { useTimerAnimation } from "../../../components/walking/UseTimerAnimation";
-import { chunkData, generateHash } from '../../../components/walking/DataProcessing';
-import { sendChunk, compareHash, resendMissingChunks } from '../../../components/walking/api/Senddata';
-import { Text, View, TouchableOpacity, Animated, StyleSheet, Modal, ActivityIndicator } from "react-native";
-
+import { icons, COLORS } from '../../../constants';
+import React, { useCallback, useState } from 'react';
+import { Sensors } from '../../../components/walking/Sensor';
+import { insertSensorData } from '../../../components/walking/SQLite';
+import { sendDatabaseFile } from '../../../components/walking/api/Senddata';
+import { useTimerAnimation } from '../../../components/walking/UseTimerAnimation';
+import {
+  chunkData,
+  generateHash,
+} from '../../../components/walking/DataProcessing';
+import {
+  sendChunk,
+  compareHash,
+  resendMissingChunks,
+} from '../../../components/walking/api/Senddata';
+import { ActivityIndicator, Modal, Animated } from 'react-native';
+import styled from 'styled-components/native';
 
 // TODO
 // 1. Save 버튼 누를시 초기화.
 
 const Walking = () => {
-  
-   // 로딩 및 모달 상태 관리
-  const [loading, setLoading] = useState(false); 
+  // 로딩 및 모달 상태 관리
+  const [loading, setLoading] = useState(false);
   const [modalVisible, setModalVisible] = useState(false); // 다이얼로그 표시 여부 관리
-  
+
   // Sensor
   const { subscribeSensors, unsubscribeSensors, sensorLog } = Sensors(25);
   // Timer Animation
-  const { seconds, setIsActive, setIsStop, formatTime, animation, Timerreset, isActive, isStop } = useTimerAnimation(2500);
-  
+  const {
+    seconds,
+    setIsActive,
+    setIsStop,
+    formatTime,
+    animation,
+    Timerreset,
+    isActive,
+    isStop,
+  } = useTimerAnimation(2500);
+
   // TODO 추후 UDP 통신 구현
   const handleSendDatabaseFile = useCallback(async () => {
     setLoading(true); // 저장 버튼 클릭 시 로딩 시작
     setModalVisible(true); // 모달 표시
-    
+
     try {
-      // chunkSize는 100000
       const chunkSize = 100000;
-      // sensorLog 값을 SHA256 해시값을 변경
       const dataHash = generateHash(sensorLog.current);
-      // chunkSize대로 데이터를 청크 단위로 나눔
       const chunks = chunkData(sensorLog.current, chunkSize);
-      
-      // 1개의 청크 단위로 데이터를 보냄.
+
       for (let i = 0; i < chunks.length; i++) {
-        const result = await sendChunk(chunks[i], i, chunks.length, "123456");
+        const result = await sendChunk(chunks[i], i, chunks.length, '123456');
         console.log(`Chunk ${i} upload status:`, result);
       }
-      
-      // 데이터를 다 보냈으면 서버 데이터 해시값과 클라이언트 데이터 해시값과 비교.
-      const compareResult = await compareHash(dataHash, "123456");
-      console.log("compareResult: ", compareResult);
-      
-      // 만약 누락된 청크가 있을시 다시 재전송 요청
+
+      const compareResult = await compareHash(dataHash, '123456');
+      console.log('compareResult: ', compareResult);
+
       if (compareResult.status === 'incomplete') {
         console.log('Resending missing chunks:', compareResult.missingChunks);
         await resendMissingChunks(compareResult.missingChunks, chunks);
       } else if (compareResult.status === 'match') {
         console.log('Data fully synchronized and verified');
-        
-        // DB파일 생성 (이떄 디비 파일은 그냥 계속 insert)
-        const TableName = "Jiseungmin"; 
+
+        const TableName = 'Jiseungmin';
         await insertSensorData(sensorLog, TableName);
-        // const result = await sendDatabaseFile(TableName); 
-        // console.log('Database file sent:', result);
-        sensorLog.current = []; // Sensor Array Initialization  
+        sensorLog.current = [];
       } else {
         console.error('Data mismatch detected');
       }
-  
     } catch (error) {
       console.error('Error during the process:', error);
-    }finally {
+    } finally {
       setLoading(false); // 로딩 중지
       setModalVisible(false); // 모달 닫기
       handleReset(); // 데이터 저장 완료 후 초기화
     }
   }, []);
 
-
-  // Start Button Click Event
   const handleStart = () => {
     setIsStop(false);
     setIsActive(true);
     subscribeSensors();
   };
 
-  // Stop Button Click Event
   const handleStop = () => {
     setIsStop(true);
     unsubscribeSensors();
     animation.stopAnimation();
   };
 
-  // Reset Button Click Event
   const handleReset = () => {
-    sensorLog.current = []; // Sensor Array Initialization
-    Timerreset(); // Timer animation Reset
+    sensorLog.current = [];
+    Timerreset();
     setIsStop(false);
     unsubscribeSensors();
     setIsActive(false);
     animation.setValue(0);
   };
 
-  // Resume Button Click Event
   const handleResume = () => {
     setIsStop(false);
     subscribeSensors();
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>{seconds > 0 ? 'Recording...' : "Ready to walk"}</Text>
-      <Text style={styles.timerText}>{formatTime()}</Text>
+    <Container>
+      <TitleText>{seconds > 0 ? 'Recording...' : 'Ready to walk'}</TitleText>
+      <TimerText>{formatTime()}</TimerText>
 
       <Modal
         transparent={true}
-        animationType="fade"  // 부드러운 fade 애니메이션으로 변경
+        animationType="fade"
         visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}  // 모달 닫기
+        onRequestClose={() => setModalVisible(false)}
       >
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalText}>Saving Data...</Text>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalText>Saving Data...</ModalText>
             {loading && (
-              <View style={styles.loaderContainer}>
+              <ActivityIndicatorContainer>
                 <ActivityIndicator size="large" color="#4C2A86" />
-              </View>
+              </ActivityIndicatorContainer>
             )}
-          </View>
-        </View>
+          </ModalContent>
+        </ModalOverlay>
       </Modal>
 
-      <View style={styles.iconContainer}>
+      <IconContainer>
         <Animated.View style={{ transform: [{ translateX: animation }] }}>
-          <icons.walking_people width={60} height={68} /> 
+          <icons.walking_people width={60} height={68} />
         </Animated.View>
-          <icons.walking_line width={320} height={10} /> 
-      </View>
+        <icons.walking_line width={320} height={10} />
+      </IconContainer>
 
       {!isActive && (
-        <TouchableOpacity style={styles.startButton} onPress={handleStart} >
-          <Text style={styles.ButtonText}>Start</Text>
-        </TouchableOpacity>
+        <StartButton onPress={handleStart}>
+          <SText>Start</SText>
+        </StartButton>
       )}
-      
-      {isActive && !isStop && (
-      <View style={styles.buttonRow}>
-            <TouchableOpacity style={styles.resetButton} onPress={handleReset} pointerEvents="auto">
-              <Text style={styles.ButtonText}>Reset</Text>
-            </TouchableOpacity>
 
-            <TouchableOpacity style={styles.stopButton} onPress={handleStop} pointerEvents="auto">
-              <Text style={styles.ButtonText}>Stop</Text>
-            </TouchableOpacity>
-      </View>
+      {isActive && !isStop && (
+        <ButtonRow>
+          <ResetButton onPress={handleReset}>
+            <ResetText>Reset</ResetText>
+          </ResetButton>
+          <StopButton onPress={handleStop}>
+            <StopText>Stop</StopText>
+          </StopButton>
+        </ButtonRow>
       )}
 
       {isActive && isStop && (
-        <View>
-          <View style={styles.buttonRow}>
-            <TouchableOpacity style={styles.resumeButton} onPress={handleResume}>
-              <Text style={styles.ButtonText}>Resume</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.resetButton} onPress={handleReset}>
-              <Text style={styles.ButtonText}>Reset</Text>
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity style={[styles.saveButton]} onPress={() => handleSendDatabaseFile()}>
-            <Text style={styles.ButtonText}>Save</Text>
-          </TouchableOpacity>
-        </View>
+        <>
+          <ButtonRow>
+            <ResumeButton onPress={handleResume}>
+              <ResumeText>Resume</ResumeText>
+            </ResumeButton>
+            <ResetButton onPress={handleReset}>
+              <ResetText>Reset</ResetText>
+            </ResetButton>
+          </ButtonRow>
+          <SaveButton onPress={handleSendDatabaseFile}>
+            <SText>Save</SText>
+          </SaveButton>
+        </>
       )}
-
-    </View>
-
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: 300,
-    padding: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  titleText: {
-    fontSize: 24,
-    color: '#333',
-    marginBottom: 20,
-  },
-  timerText: {
-    fontSize: 54,
-    color: '#000'
-  },
-  iconContainer: {
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    marginBottom: 50,
-    marginTop: 100
-  },
-  runningIcon: {
-    width: 60,
-    height: 68
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  startButton: {
-    backgroundColor: '#4C2A86',
-    borderRadius: 30,
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    width:"90%",
-    marginBottom: 20
-  },
-  ButtonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  resetButton: {
-    backgroundColor: '#4C2A86',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 30
-  },
-  stopButton: {
-    backgroundColor: '#4C2A86',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 30,
-    marginLeft: 100
-  },
-  resumeButton: {
-    backgroundColor: '#4C2A86',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 30,
-    marginRight: 90
-  },
-  saveButton: {
-    backgroundColor: '#4C2A86',
-    paddingVertical: 15,
-    paddingHorizontal: 160,
-    borderRadius: 30,
-    marginTop: 20,
-  },
-  
-  
-});
-
 export default Walking;
+
+// styled-components
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${COLORS.white};
+`;
+
+const ModalOverlay = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.View`
+  width: 300px;
+  padding: 20px;
+  background-color: ${COLORS.white};
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalText = styled.Text`
+  font-size: 18px;
+  `;
+
+const ActivityIndicatorContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleText = styled.Text`
+  font-size: 24px;
+  color: ${COLORS.black};
+  margin-bottom: 20px;
+`;
+
+const TimerText = styled.Text`
+  font-size: 54px;
+  color: ${COLORS.black};
+`;
+
+const IconContainer = styled.View`
+  justify-content: center;
+  align-items: flex-start;
+  margin-bottom: 50px;
+  margin-top: 100px;
+`;
+
+const ButtonRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  gap: 55px;
+`;
+
+const StartButton = styled.TouchableOpacity`
+  background-color: #4c2a86;
+  border-radius: 30px;
+  padding: 15px 40px;
+  width: 90%;
+  margin-bottom: 20px;
+  shadow-color: ${COLORS.black};
+  shadow-opacity: 0.4;
+  shadow-radius: 5px;
+  shadow-offset: 0px 3px;
+`;
+
+const SText = styled.Text`
+  color: ${COLORS.white};
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const StopText = styled.Text`
+  color: ${COLORS.white};
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const ResetText = styled.Text`
+  color: ${COLORS.soft_blue};
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const ResumeText = styled.Text`
+  color: ${COLORS.dark_indigo};
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const ResetButton = styled.TouchableOpacity`
+  background-color: ${COLORS.white};
+  padding: 15px 40px;
+  border-radius: 30px;
+  width: 154px;
+  shadow-color: ${COLORS.black};
+  shadow-opacity: 0.4;
+  shadow-radius: 5px;
+  shadow-offset: 0px 3px;
+`;
+
+const StopButton = styled.TouchableOpacity`
+  background-color: ${COLORS.soft_blue};
+  padding: 15px 40px;
+  border-radius: 30px;
+  width: 154px;
+  shadow-color: ${COLORS.black};
+  shadow-opacity: 0.4;
+  shadow-radius: 5px;
+  shadow-offset: 0px 3px;
+`;
+
+const ResumeButton = styled.TouchableOpacity`
+  background-color: ${COLORS.white};
+  padding: 15px 40px;
+  border-radius: 30px;
+  width: 154px;
+  shadow-color: ${COLORS.black};
+  shadow-opacity: 0.4;
+  shadow-radius: 5px;
+  shadow-offset: 0px 3px;
+`;
+
+const SaveButton = styled.TouchableOpacity`
+  background-color: #4c2a86;
+  padding: 15px 160px;
+  border-radius: 30px;
+  margin-top: 20px;
+  shadow-color: ${COLORS.black};
+  shadow-opacity: 0.4;
+  shadow-radius: 5px;
+  shadow-offset: 0px 3px;
+`;
