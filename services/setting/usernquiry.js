abba9e22e84455c13c71a9f0b7e07a3222d254ff
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-11-27 14:09:37 +0900

    fix: 프로젝트 참여

--- services/setting/usernquiry.js
+++ services/setting/usernquiry.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.0.9:4000'; // 서버 주소 설정
+const API_BASE_URL = 'http://192.168.25.38:4000'; // 서버 주소 설정
 
 const api = axios.create({
   baseURL: API_BASE_URL, 
